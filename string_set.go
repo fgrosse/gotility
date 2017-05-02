3e2732676f0ede3504554360cf3d1472e9882b5f
Friedrich Große <friedrich@fraugster.com>
2017-05-02 12:51:11 +0200

    Implement AddAll

--- string_set.go
+++ string_set.go
@@ -19,6 +19,13 @@ func (s StringSet) Set(value string) {
 	s[value] = struct{}{}
 }
 
+// SetAll registers all values in this hash set.
+func (s StringSet) SetAll(value []string) {
+	for _, v := range value {
+		s.Set(v)
+	}
+}
+
 // Contains returns true if the given value is contained in this string set.
 func (s StringSet) Contains(value string) bool {
 	_, exists := s[value]
